39ba48738c0d5854d306c431cb0f89fc3223f88a
Seraya Maouche <seraya.maouche@iscb.org>
2017-03-06 19:07:45 +0100

    Update RGooglePlus.R

--- RGooglePlus.R
+++ RGooglePlus.R
@@ -1,17 +1,19 @@
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Script File: RGooglePlus.R  
-# Date of creation: 12 Nov  2016
+# Date of creation: 12 Nov 2016
 # Date of last modification: 25 Feb 2017
 # Author: Seraya Maouche <seraya.maouche@iscb.org>
 # Short Description: This script provides an interface to Google+ API
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# CRAN : https://cran.r-project.org/web/packages/plusser/index.html
+# Github : https://github.com/cran/plusser
 
 # Load required packages
 library(RCurl)
 
 #********************** Installing plusser package from CRAN
 # install.packages("plusser")
-#
 
 #********************** Load library
 library(plusser)
@@ -34,8 +36,13 @@ users.p <- harvestActivity(myKey, "plusoners")
 # harvestPage(): Retrieve the posts of a user’s G+ page
 # This function retrieves the most recent posts that a user put on his page. Google calls this ‘listing
 # activities‘.
-myPosts.df <- harvestPage("115484368398593587193")
-gPosts.df  <- harvestPage("+google", results=20)
+myposts=harvestPage("+[name]", parseFun = parsePost, results = 1, nextToken = NULL, cr = 1)
+str(myposts)
+plot(myposts$ti,myposts$nR)
+plot(myposts$ti,myposts$nC)
+myPosts.df = harvestPage("115484368398593587193")
+gPosts.df  = harvestPage("+google", results=20)
+
 
 # Searching for Google+ Posts
 searchPost("#genomics")
